1ad50bfb9d9cd600e4370b2bc44ac95607578ca7
Sasarino MARi <develop.mari@gmail.com>
2020-09-16 11:40:23 +0900

    게시물 리스트에서 작성하는 자바스크립트 코드 추가, 게시물 뷰에서 동작하는 자바스크립트 코드 수정

---
+++ AF.Moa/Macro/Scripts/BoardListPlus.js
@@ -0,0 +1,6 @@
+﻿var n=$('.no img');for(var i=0;i<n.length;i++)var x=n[i].parentElement.parentElement.outerText="";
+var c=$('td a');for(var i=0;i<(c.length>9?9:c.length);i++)var x=c[i].innerHTML="<font style='color:red'><b>"+(i+1)+".</b></font> "+c[i].innerHTML;
+var x=document.onkeydown=function(eo){var e=eo?eo:window.event;
+    if(e.ctrlKey!=true||e.shiftKey!=true)return; // 조합키 체크
+    var kc=e.keyCode-49;if(0<=kc&&kc<10)c[kc].click(); //게시물로 이동
+};
--- AF.Moa/Macro/Scripts/BoardViewHotKey.js
+++ AF.Moa/Macro/Scripts/BoardViewHotKey.js
@@ -1,12 +1,19 @@
-﻿var a = $('.listTb tbody a');
-var l = a.length;
-function cvt(c) { return c.charCodeAt() - 32; }
-var x = document.onkeydown = function (eo) {
-    var e = eo ? eo : window.event;
-    if (e.ctrlKey != true || e.shiftKey != true) return; // 조합키 체크
-    if (e.keyCode == cvt('z')) a[l - 1].click();// 다음글(z키)
-    if (e.keyCode == cvt('x') && l > 1) a[0].click();// 이전글(x키)
-    if (e.keyCode == cvt('c')) $('#comment').focus();//덧글창으로
-    if (e.keyCode == 13) $('.commentWrite input')[2].click();//등록버튼
-    //if(e.keyCode==cvt('r'))location.reload();//새로고침
+﻿var a=$('.listTb tbody a');
+var l=a.length;
+function cvt(c){return c.charCodeAt()-32;}
+var x=document.onkeydown=function(eo){var e=eo?eo:window.event;
+    if(e.ctrlKey!=true||e.shiftKey!=true)return; // 조합키 체크
+    if(e.keyCode==cvt('z'))a[l-1].click();// 다음글(z키)
+    if(e.keyCode==cvt('x')&&l>1)a[0].click();// 이전글(x키)
+    if(e.keyCode==cvt('c'))$('#comment').focus();//덧글창으로
+    if(e.keyCode==cvt('q'))jf_goList();//글 목록으로
+    if(e.keyCode==13)$('.commentWrite input')[2].click();//등록버튼
 };
+function format(s, k) { return "<font style='color:red'><b>"+k+".</b></font> "+s;}
+if (a.length == 2) {
+    var x = a[0].innerHTML = format(a[0].innerHTML, "X");
+    var x = a[1].innerHTML = format(a[1].innerHTML, "Z");
+}
+else {
+    var x = a[0].innerHTML = format(a[0].innerHTML, "Z");
+}
